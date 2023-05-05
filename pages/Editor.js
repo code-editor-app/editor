8423434b1c9443e78b613f42059ebe98860ed871
Aman Saxena <65945317+amansaxena1@users.noreply.github.com>
2023-05-05 17:07:45 +0530

    added buttons

--- pages/Editor.js
+++ pages/Editor.js
@@ -53,14 +53,20 @@ const Editor = () => {
     'share-outline',
   ];
 
+  const numbers = ['0','1','2','3','4','5','6','7','8','9'];
+  
+  const line1 = [{lower: 'q', upper: 'Q'},{lower: 'w', upper: 'W'},{lower: 'e', upper: 'E'},{lower: 'r', upper: 'R'},{lower: 't', upper: 'T'},{lower: 'y', upper: 'Y'},{lower: 'u', upper: 'U'},{lower: 'i', upper: 'I'},{lower: 'o', upper: 'O'},{lower: 'p', upper: 'P'}]; 
+  const line2 = [{lower: 'a', upper: 'A'},{lower: 's', upper: 'S'},{lower: 'd', upper: 'D'},{lower: 'f', upper: 'F'},{lower: 'g', upper: 'G'},{lower: 'h', upper: 'H'},{lower: 'j', upper: 'J'},{lower: 'k', upper: 'K'},{lower: 'l', upper: 'L'}];
+  const line3 = [{lower: 'z', upper: 'Z'},{lower: 'x', upper: 'X'},{lower: 'c', upper: 'C'},{lower: 'v', upper: 'V'},{lower: 'b', upper: 'B'},{lower: 'n', upper: 'N'},{lower: 'm', upper: 'M'},{lower: '<=', upper: '<='}];
   return (
     <View
       style={{
         backgroundColor: '#363636',
         height: height,
       }}>
-      {/* <CodeEditor
+      <CodeEditor
         style={{
+          height: 200,
           fontSize: 10,
           inputLineHeight: 13,
           highlighterLineHeight: 13,
@@ -68,25 +74,28 @@ const Editor = () => {
         language="cpp"
         syntaxStyle={CodeEditorSyntaxStyles.androidstudio}
         showLineNumbers
-    /> */}
-      <ScrollView
-        horizontal={true}
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-        }}
-    >
-        {editorIcons.map((symbol, index) => {
+      />
+      <View style={{
+        height: 38,
+      }}>
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{
+            width: '100%',
+            justifyContent: 'space-between',
+          }}>
+          {editorIcons.map((symbol, index) => {
           return (
             <TouchableOpacity
               style={{
                 backgroundColor: '#404040',
-                width: 40,
-                height: 40,
+                flex: 1,
+                height: 35,
                 justifyContent: 'center',
                 alignItems:'center',
-                borderRadius: 3,
-                marginHorizontal:1
+                borderRadius: 5,
+                marginHorizontal: 2,
               }}
               key={index}>
               <Ionicons
@@ -97,42 +106,211 @@ const Editor = () => {
               />
             </TouchableOpacity>
           );
-        })}
-      </ScrollView>
-      <ScrollView
-        horizontal={true}
-        style={{
-          display: 'flex',
-          flexDirection: 'row',
-        }}>
-        {shortcutSymbols.map((symbol, index) => {
+          })}
+        </ScrollView>
+      </View>
+      <View style={{
+        height: 38,
+      }}>
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{
+            height: 30,
+          }}>
+          {shortcutSymbols.map((symbol, index) => {
+            return (
+              <TouchableOpacity
+                style={{
+                  backgroundColor: '#404040',
+                  width: 30,
+                  height: 30,
+                  justifyContent: 'center',
+                  borderRadius: 5,
+                  marginHorizontal: 2,
+                }}
+                key={index}>
+                <Text
+                  key={index}
+                  style={{
+                    color: 'white',
+                    margin: 0,
+                    textAlign: 'center',
+                    fontFamily: 'monospace',
+                    fontSize: 17,
+                    marginBottom: 2,
+                  }}>
+                  {symbol}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
+        </ScrollView>
+      </View>
+      <View style={{
+        height: 38,
+      }}>
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{
+            width: '100%',
+            justifyContent: 'space-between',
+          }}>
+          {numbers.map((symbol, index) => {
           return (
             <TouchableOpacity
               style={{
                 backgroundColor: '#404040',
+                
+                height: 35,
                 width: 30,
-                height: 30,
                 justifyContent: 'center',
-                borderRadius: 3,
-                marginHorizontal: 1,
+                alignItems:'center',
+                borderRadius: 5,
+                marginHorizontal: 2,
               }}
               key={index}>
               <Text
-                key={index}
-                style={{
-                  color: 'white',
-                  margin: 0,
-                  textAlign: 'center',
-                  fontFamily: 'monospace',
-                  fontSize: 17,
-                  marginBottom: 2,
-                }}>
-                {symbol}
-              </Text>
+                  key={index}
+                  style={{
+                    color: 'white',
+                    margin: 0,
+                    textAlign: 'center',
+                    fontFamily: 'monospace',
+                    fontSize: 17,
+                    marginBottom: 2,
+                  }}>
+                  {symbol}
+                </Text>
             </TouchableOpacity>
           );
-        })}
-      </ScrollView>
+          })}
+        </ScrollView>
+      </View>
+      <View style={{
+        height: 38,
+      }}>
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{
+            width: '100%',
+            justifyContent: 'space-between',
+          }}>
+          {line1.map((symbol, index) => {
+          return (
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#404040',
+                
+                height: 35,
+                width: 30,
+                justifyContent: 'center',
+                alignItems:'center',
+                borderRadius: 5,
+                marginHorizontal: 2,
+              }}
+              key={index}>
+              <Text
+                  key={index}
+                  style={{
+                    color: 'white',
+                    margin: 0,
+                    textAlign: 'center',
+                    fontFamily: 'monospace',
+                    fontSize: 17,
+                    marginBottom: 2,
+                  }}>
+                  {symbol.lower}
+                </Text>
+            </TouchableOpacity>
+          );
+          })}
+        </ScrollView>
+      </View>
+      <View style={{
+        height: 38,
+      }}>
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{
+            width: '100%',
+            justifyContent: 'space-between',
+          }}>
+          {line2.map((symbol, index) => {
+          return (
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#404040',
+                
+                height: 35,
+                width: 30,
+                justifyContent: 'center',
+                alignItems:'center',
+                borderRadius: 5,
+                marginHorizontal: 2,
+              }}
+              key={index}>
+              <Text
+                  key={index}
+                  style={{
+                    color: 'white',
+                    margin: 0,
+                    textAlign: 'center',
+                    fontFamily: 'monospace',
+                    fontSize: 17,
+                    marginBottom: 2,
+                  }}>
+                  {symbol.lower}
+                </Text>
+            </TouchableOpacity>
+          );
+          })}
+        </ScrollView>
+      </View>
+      <View style={{
+        height: 38,
+      }}>
+        <ScrollView
+          horizontal={true}
+          showsHorizontalScrollIndicator={false}
+          contentContainerStyle={{
+            width: '100%',
+            justifyContent: 'space-between',
+          }}>
+          {line3.map((symbol, index) => {
+          return (
+            <TouchableOpacity
+              style={{
+                backgroundColor: '#404040',
+                
+                height: 35,
+                width: 30,
+                justifyContent: 'center',
+                alignItems:'center',
+                borderRadius: 5,
+                marginHorizontal: 2,
+              }}
+              key={index}>
+              <Text
+                  key={index}
+                  style={{
+                    color: 'white',
+                    margin: 0,
+                    textAlign: 'center',
+                    fontFamily: 'monospace',
+                    fontSize: 17,
+                    marginBottom: 2,
+                  }}>
+                  {symbol.lower}
+                </Text>
+            </TouchableOpacity>
+          );
+          })}
+        </ScrollView>
+      </View>
     </View>
   );
 };
